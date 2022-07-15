83f7841454a95fb26033a66825990b72f6227b6b
TuanDao2002 <tuan09092002@gmail.com>
2022-07-15 10:29:33 +0700

    first commit

---
+++ COSC2659_Assignment1_s3877347/COSC2659_Assignment1_s3877347App.swift
@@ -0,0 +1,17 @@
+//
+//  COSC2659_Assignment1_s3877347App.swift
+//  COSC2659_Assignment1_s3877347
+//
+//  Created by Tuan Dao on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct COSC2659_Assignment1_s3877347App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COSC2659_Assignment1_s3877347/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COSC2659_Assignment1_s3877347
+//
+//  Created by Tuan Dao on 15/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
